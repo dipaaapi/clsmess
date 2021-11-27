e09068a34f0524ece8a8c9e2964eedbee8bedaa9
dipaaapi <31286911+dipaaapi@users.noreply.github.com>
2021-11-27 21:18:02 +0800

    first commit

---
+++ database/migrations/2021_11_27_054723_user_info.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UserInfo extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('userinfo', function (Blueprint $table) {
+            $table->id();
+            $table->integer('mobile')->nullable();
+            $table->string('address')->nullable();
+            $table->text('facebook');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('userinfo');
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<x-guest-layout>
+
+    <div class="items-top justify-center bg-gray-100 dark:bg-gray-900 sm:items-center">
+        @if (Route::has('login'))
+            <div class="fixed top-0 right-0 px-6 py-4 sm:block">
+                @auth
+                    <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                @else
+                    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                    @endif
+                @endauth
+            </div>
+        @endif
+    </div>
+    <p class="m-auto w-max h-auto">Welcome to My Personal Website</p>
+
+</x-guest-layout>
